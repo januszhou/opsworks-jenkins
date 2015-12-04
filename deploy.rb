466649cf0c318bf18910d73d81b6adf0f5d0e651
Janus <janus.zhou1005@gmail.com>
2015-12-04 16:47:49 -0500

    Make single quote

--- deploy.rb
+++ deploy.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'json'
 
-result = `aws opsworks --region us-east-1 create-deployment --stack-id 582fe17a-020f-44e9-9af6-57935f97d016 --command "{\"Name\":\"execute_recipes\", \"Args\":{\"recipes\":[\"deploy_setup::deploy\"]}}"`
+result = `aws opsworks --region us-east-1 create-deployment --stack-id 582fe17a-020f-44e9-9af6-57935f97d016 --command '{\"Name\":\"execute_recipes\", \"Args\":{\"recipes\":[\"deploy_setup::deploy\"]}}'`
 puts result
 
 
