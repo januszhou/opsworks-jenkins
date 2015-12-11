bde439f91fd28b2ad94a25a2affa4ae821978328
Janus <janus.zhou1005@gmail.com>
2015-12-11 14:31:48 -0500

    Make jenkins to create c3.large

--- auto-scaling.rb
+++ auto-scaling.rb
@@ -7,7 +7,7 @@ summary = JSON.parse(result)
 status = summary['StackSummary']['InstancesCount']
 
 def createLoadBasedInstance()
-    result = `aws opsworks --region us-east-1 create-instance --stack-id 582fe17a-020f-44e9-9af6-57935f97d016 --layer-id e13e9dfd-4b78-4736-8e9a-429ca8310749 --instance-type t2.micro --auto-scaling-type load --os "Amazon Linux 2015.09" --ssh-key-name "CodeDeploy-server"`
+    result = `aws opsworks --region us-east-1 create-instance --stack-id 582fe17a-020f-44e9-9af6-57935f97d016 --layer-id e13e9dfd-4b78-4736-8e9a-429ca8310749 --instance-type c3.large --auto-scaling-type load --os "Amazon Linux 2015.09" --ssh-key-name "CodeDeploy-server"`
     json = JSON.parse(result)
     if json['InstanceId']
         return true
