3ef157147ba4cb563495be1f1506340013bee02a
Janus <janus.zhou1005@gmail.com>
2015-12-28 22:07:53 -0500

    For crave premium 2 layer

---
+++ auto-scaling-crave-premium-2.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'json'
+
+result = `aws opsworks --region us-east-1 describe-stack-summary --stack-id 582fe17a-020f-44e9-9af6-57935f97d016`
+
+summary = JSON.parse(result)
+status = summary['StackSummary']['InstancesCount']
+
+def createLoadBasedInstance()
+    result = `aws opsworks --region us-east-1 create-instance --stack-id 582fe17a-020f-44e9-9af6-57935f97d016 --layer-id 279384b6-d15f-493a-b06e-581d9f99c892 --instance-type r3.4xlarge --auto-scaling-type load --os "Amazon Linux 2015.09" --ssh-key-name "CodeDeploy-server"`
+    json = JSON.parse(result)
+    if json['InstanceId']
+        return true
+    else
+        return false
+    end
+end
+
+if status['Stopped'] == nil
+    createLoadBasedInstance()
+    puts "Create new load based instance"
+else 
+    puts "Already has #{status['Stopped']} stopped load instances, no need for new one"
+end
+
