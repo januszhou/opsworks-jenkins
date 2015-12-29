98fa98193284a12ea21f36d4c67466f1a7a695f6
Janus <janus.zhou1005@gmail.com>
2015-12-28 23:42:08 -0500

    Use different way to count stopped instance

--- auto-scaling-crave-premium-2.rb
+++ auto-scaling-crave-premium-2.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'json'
 
-result = `aws opsworks --region us-east-1 describe-stack-summary --stack-id 582fe17a-020f-44e9-9af6-57935f97d016`
+result = `aws opsworks --region us-east-1 describe-instances --layer-id 279384b6-d15f-493a-b06e-581d9f99c892`
 
 summary = JSON.parse(result)
 status = summary['StackSummary']['InstancesCount']
@@ -16,10 +16,22 @@ def createLoadBasedInstance()
     end
 end
 
-if status['Stopped'] == nil
+# log online instance, log 
+status = {'online' => 0,  'stopped' => 0}
+
+for instance in instances
+    if instance['Status'] == 'online'
+        status['online'] += 1
+    end
+
+    if instance['Status'] == 'stopped'
+        status['stopped'] += 1
+    end
+end
+
+if status['stopped'] == 0
     createLoadBasedInstance()
     puts "Create new load based instance"
 else 
-    puts "Already has #{status['Stopped']} stopped load instances, no need for new one"
+    puts "Already has #{status['stopped']} stopped load instances, no need for new one"
 end
-
--- auto-scaling.rb
+++ auto-scaling.rb
@@ -1,10 +1,10 @@
 require 'rubygems'
 require 'json'
 
-result = `aws opsworks --region us-east-1 describe-stack-summary --stack-id 582fe17a-020f-44e9-9af6-57935f97d016`
+result = `aws opsworks --region us-east-1 describe-instances --layer-id e13e9dfd-4b78-4736-8e9a-429ca8310749`
 
 summary = JSON.parse(result)
-status = summary['StackSummary']['InstancesCount']
+instances = summary['Instances']
 
 def createLoadBasedInstance()
     result = `aws opsworks --region us-east-1 create-instance --stack-id 582fe17a-020f-44e9-9af6-57935f97d016 --layer-id e13e9dfd-4b78-4736-8e9a-429ca8310749 --instance-type c3.large --auto-scaling-type load --os "Amazon Linux 2015.09" --ssh-key-name "CodeDeploy-server"`
@@ -16,10 +16,23 @@ def createLoadBasedInstance()
     end
 end
 
-if status['Stopped'] == nil
+# log online instance, log 
+status = {'online' => 0,  'stopped' => 0}
+
+for instance in instances
+    if instance['Status'] == 'online'
+        status['online'] += 1
+    end
+
+    if instance['Status'] == 'stopped'
+        status['stopped'] += 1
+    end
+end
+
+if status['stopped'] == 0
     createLoadBasedInstance()
     puts "Create new load based instance"
 else 
-    puts "Already has #{status['Stopped']} stopped load instances, no need for new one"
+    puts "Already has #{status['stopped']} stopped load instances, no need for new one"
 end
 
